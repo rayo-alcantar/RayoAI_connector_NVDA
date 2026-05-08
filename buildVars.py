3b1d112c8514489ad5727747cd8fe13c510e8941
Angel Alcantar <rayoalcantar@gmail.comgit>
2026-05-07 20:16:57 -0600

    Release 2026.5

--- buildVars.py
+++ buildVars.py
@@ -29,11 +29,9 @@ addon_info = {
 	# Author(s)
 	"addon_author": "Angel Alcántar <rayoalcantar@gmail.com>",
 	# URL for the add-on documentation support
-	"addon_url": "https://github.com/rayo-alcantar/RayoAI_connector_NVDA"
-",
+	"addon_url": "https://github.com/rayo-alcantar/RayoAI_connector_NVDA",
 	# URL for the add-on repository where the source code can be found
-	"addon_sourceURL": "https://github.com/rayo-alcantar/RayoAI_connector_NVDA
-",
+	"addon_sourceURL": "https://github.com/rayo-alcantar/RayoAI_connector_NVDA",
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
